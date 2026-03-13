d26f9f93aaf28400ad669b4461b1097df87f32c7
Sathwikg03 <gangapuramsathwikram@gmail.com>
2026-03-13 18:25:02 +0530

    Update settings.py

--- luxe_backend/luxe_backend/settings.py
+++ luxe_backend/luxe_backend/settings.py
@@ -129,8 +129,8 @@ CSRF_TRUSTED_ORIGINS = [
 
 # ── Email ─────────────────────────────────────────────────────────────────────
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST          = 'smtp.gmail.com'
-EMAIL_PORT          = 587
+EMAIL_HOST          = config('EMAIL_HOST', default='smtp-relay.brevo.com')
+EMAIL_PORT          = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS       = True
 EMAIL_HOST_USER     = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
