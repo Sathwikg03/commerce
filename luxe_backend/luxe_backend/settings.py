0253e17c8aa20f532d20087167a690cbb82e962a
Sathwikg03 <gangapuramsathwikram@gmail.com>
2026-03-13 18:49:32 +0530

    Update settings.py

--- luxe_backend/luxe_backend/settings.py
+++ luxe_backend/luxe_backend/settings.py
@@ -129,11 +129,12 @@ CSRF_TRUSTED_ORIGINS = [
 # ── Email ─────────────────────────────────────────────────────────────────────
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = config('EMAIL_HOST', default='smtp-relay.brevo.com')
-EMAIL_PORT          = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS       = True
+EMAIL_PORT          = config('EMAIL_PORT', default=465, cast=int)
+EMAIL_USE_TLS       = False   # ← off for SSL
+EMAIL_USE_SSL       = True    # ← use SSL instead
 EMAIL_HOST_USER     = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', 
+DEFAULT_FROM_EMAIL  = config('DEFAULT_FROM_EMAIL',
     default=f'Luxe Store <{config("EMAIL_HOST_USER", default="")}>')
 EMAIL_TIMEOUT       = 10
 
